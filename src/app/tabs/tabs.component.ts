271c7a7958df1f41a2dd12803c235ade71bf7e5b
Andrii Mutylo <amutylo@gmail.com>
2018-01-05 13:01:16 +0200

    first step added components and overall styles

---
+++ src/app/tabs/tabs.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.css']
+})
+export class TabsComponent implements OnInit {
+  characters = [
+    {name:  'Luke Skywalker', side: ''},
+    {name:  'Dart Weider', side: ''}
+  ];
+  chosenList = 'all'
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onChoose(side) {
+    this.chosenList = side;
+  }
+
+  getCharacters() {
+    let filtered;
+    if (this.chosenList === 'all') {
+      filtered = this.characters.slice();
+      return filtered;
+    }
+    filtered = this.characters.filter((char) => {
+      return char.side === this.chosenList;
+    })
+    return filtered;
+  }
+}
