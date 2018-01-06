6771a7115f894bf2e05ab6a8f28b1d7dabaafed8
Andrii Mutylo <amutylo@gmail.com>
2018-01-06 17:50:30 +0200

    Added service dependency injection

---
+++ src/app/star-wars.service.ts
@@ -0,0 +1,26 @@
+export class StarWarsService {
+  private characters = [
+    { name: 'Luke Skywalker', side: '' },
+    { name: 'Darth Vader', side: '' }
+  ];
+
+  /**
+   * get all & filtered characters in array
+   * @param chosenList;
+   */
+  getCharacters(chosenList) {
+    if (chosenList === 'all') {
+      return this.characters.slice();
+    }
+    return this.characters.filter((char) => {
+      return char.side === chosenList;
+    })
+  }
+
+  onSideChosen(charInfo) {
+    const pos = this.characters.findIndex((char) => {
+      return char.name === charInfo.name;
+    })
+    this.characters[pos].side = charInfo.side;
+  }
+}
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StarWarsService } from '../star-wars.service';
+
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.css']
+  styleUrls: ['./tabs.component.css'],
+  providers: [StarWarsService]
 })
 export class TabsComponent implements OnInit {
-  characters = [
-    {name:  'Luke Skywalker', side: ''},
-    {name:  'Dart Weider', side: ''}
-  ];
-  chosenList = 'all'
-  constructor() { }
+  characters = [];
+  chosenList = 'all';
+  swService: StarWarsService;
+
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
 
   ngOnInit() {
   }
@@ -21,22 +25,7 @@ export class TabsComponent implements OnInit {
   }
 
   getCharacters() {
-    let filtered;
-    if (this.chosenList === 'all') {
-      filtered = this.characters.slice();
-      return filtered;
-    }
-    filtered = this.characters.filter((char) => {
-      return char.side === this.chosenList;
-    })
-    return filtered;
-  }
-
-  onSideChosen(charInfo) {
-    const pos = this.characters.findIndex((char) => {
-      return char.name === charInfo.name;
-    })
-
-    this.characters[pos].side = charInfo.side;
+    this.characters = this.swService.getCharacters(this.chosenList);
+    return this.characters;
   }
 }
