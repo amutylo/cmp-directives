cab6b6f4ee1ce850351cab482b98ed53a2c0725e
Andrii Mutylo <amutylo@gmail.com>
2018-01-14 15:40:34 +0200

    update reactivity

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StarWarsService } from 'app/star-wars.service';
+import { Subscription } from 'rxjs/Subscription';
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
   characters = [];
   activatedRoute: ActivatedRoute;
   swService: StarWarsService;
   loadedSide = 'all';
+  subscription;
 
   constructor(activatedRoute: ActivatedRoute, swService: StarWarsService) {
     this.activatedRoute = activatedRoute;
@@ -26,10 +29,14 @@ export class ListComponent implements OnInit {
       }
     );
 
-    this.swService.charactersChanged.subscribe(
+    this.subscription = this.swService.charactersChanged.subscribe(
       () => {
         this.characters = this.swService.getCharacters(this.loadedSide);
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
